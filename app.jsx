8afa12b789a25b3d6a9b62a52d196fef8a5cf5df
Graham Cottridge <codinggc@gmail.com>
2018-05-11 20:24:27 +0100

    Add header and player components

---
+++ app.jsx
@@ -0,0 +1,35 @@
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>{props.title}</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Graham
+          </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 31 </div>
+                <button className="counter-action increment"> + </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+  title: "Scoreboard",
+};
+
+
+ReactDOM.render(<Application />, document.getElementById('container'));
